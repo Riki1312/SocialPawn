6c232f21bd1716bb72390f2dcb778317807412e3
CostaMatteo <m.costamagna.9908@vallauri.edu>
2018-05-24 14:27:13 +0200

    U

--- BrCs/Ajax.php
+++ BrCs/Ajax.php
@@ -91,7 +91,7 @@ function UpdatePosts(){
     $obj = json_decode($_REQUEST["data"], true);
     $User = new Components();
 
-    //$GLOBALS['data'] = json_encode($User->UpdatePost($obj['idPosts']));
+    $GLOBALS['data'] = json_encode($User->UpdatePost($obj['idPosts']));
 }
 
 /* --- */
